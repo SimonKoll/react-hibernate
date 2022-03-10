574e378e4659b6917c007e369b59255b79cb5f2b
SimonKoll <simonkoll20@gmail.com>
2022-03-10 08:37:16 +0100

    commit 2

--- src/test/java/at/htlleonding/mutiny/MutinyScratchPad.java
+++ src/test/java/at/htlleonding/mutiny/MutinyScratchPad.java
@@ -2,12 +2,10 @@ package at.htlleonding.mutiny;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.infrastructure.Infrastructure;
-import io.smallrye.mutiny.vertx.codegen.lang.ConstructorWithObjectDelegateCodeWriter;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.wildfly.common.Assert;
 
-import java.lang.invoke.LambdaConversionException;
 import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,12 +20,11 @@ public class MutinyScratchPad {
     final Integer theAnswer = 42;
 
     void printWithThreadId(String msg) {
-        System.out.println("Thread: "+Thread.currentThread().getName() + " " + msg);
+        System.out.println("Thread: " + Thread.currentThread().getName() + " " + msg);
     }
 
     @Test
-    public void uniFromItem_subscribe_retrieveItem()
-    {
+    public void uniFromItem_subscribe_retrieveItem() {
         Uni<Integer> u = Uni.createFrom().item(() -> 1);
         var result = u.subscribe().with(item -> integerResult = item);
 
@@ -35,13 +32,15 @@ public class MutinyScratchPad {
     }
 
     @Test
-    public void uniFromItem_addEventReactions_retrieveItem()
-    {
+    public void uniFromItem_addEventReactions_retrieveItem() {
         Uni<Integer> u1 = Uni.createFrom().item(() -> 1);
-        var u2 = u1.onItem().invoke(()->booleans[0] = true);
+        var u2 = u1.onItem().invoke(() -> booleans[0] = true);
         var u3 = u2.onSubscription().invoke(() -> booleans[1] = true);
 
-        var result = u3.subscribe().with(item -> {booleans[2] = true; integerResult = item;});
+        var result = u3.subscribe().with(item -> {
+            booleans[2] = true;
+            integerResult = item;
+        });
 
         Assertions.assertEquals(1, integerResult);
         Assert.assertTrue(booleans[0]);
@@ -50,12 +49,16 @@ public class MutinyScratchPad {
     }
 
     @Test
-    public void uniFromItem_throwsException_onFailure()
-    {
+    public void uniFromItem_throwsException_onFailure() {
         Uni<Integer> u =
-                Uni.createFrom().item(() -> { throw new IllegalArgumentException("Boom"); } );
+                Uni.createFrom().item(() -> {
+                    throw new IllegalArgumentException("Boom");
+                });
 
-        var u2 = u.onFailure().recoverWithItem(f -> {hasFailed = true; return -2;});
+        var u2 = u.onFailure().recoverWithItem(f -> {
+            hasFailed = true;
+            return -2;
+        });
 
         integerResult = u2.await().atMost(Duration.ofMinutes(1));
 
@@ -68,9 +71,9 @@ public class MutinyScratchPad {
         Thread.sleep(250);
         return 42;
     }
+
     @Test
-    public void uniFromProducer_delayed_retrieveItem()
-    {
+    public void uniFromProducer_delayed_retrieveItem() {
         Uni<Integer> u1 = Uni.createFrom().emitter(em -> {
             try {
                 em.complete(produceInt());
@@ -80,11 +83,14 @@ public class MutinyScratchPad {
             }
         });
 
-        var u2 = u1.onItem().invoke(()->booleans[0] = true);
+        var u2 = u1.onItem().invoke(() -> booleans[0] = true);
         var u3 = u2.onSubscription().invoke(() -> booleans[1] = true);
 
         var startAt = System.currentTimeMillis();
-        var result = u3.subscribe().with(item -> {booleans[2] = true; integerResult = item;});
+        var result = u3.subscribe().with(item -> {
+            booleans[2] = true;
+            integerResult = item;
+        });
         var finishAt = System.currentTimeMillis();
 
         Assertions.assertEquals(theAnswer, integerResult);
@@ -95,8 +101,7 @@ public class MutinyScratchPad {
     }
 
     @Test
-    public void uniFromProducer_delayed_block_retrieveItem()
-    {
+    public void uniFromProducer_delayed_block_retrieveItem() {
         Uni<Integer> u1 = Uni.createFrom().emitter(em -> {
             try {
                 em.complete(produceInt());
@@ -106,7 +111,7 @@ public class MutinyScratchPad {
             }
         });
 
-        var u2 = u1.onItem().invoke(()->booleans[0] = true);
+        var u2 = u1.onItem().invoke(() -> booleans[0] = true);
         var u3 = u2.onSubscription().invoke(() -> booleans[1] = true);
 
         integerResult = u3.await().indefinitely();
@@ -118,38 +123,47 @@ public class MutinyScratchPad {
 
     ThreadFactory threadFactory = new NameableThreadFactory("EMIT_ON_THREAD");
     ExecutorService executor = Executors.newFixedThreadPool(10, threadFactory);
+
     @Test
-    public void uniFromProducer_executeOnAnotherThread_delayed_retrieveItem()
-    {
+    public void uniFromProducer_executeOnAnotherThread_delayed_retrieveItem() {
         //specify the work to do
         Uni<Integer> u1 = Uni.createFrom()
                 .emitter(em -> {
-            try {
-                em.complete(produceInt());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                em.fail(e);
-            }
-        });
+                    try {
+                        em.complete(produceInt());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        em.fail(e);
+                    }
+                });
 
         //tell it to run a subscription on another thread
         var u2 = u1
-                .runSubscriptionOn(executor)
-                ;
+                .runSubscriptionOn(executor);
 
         //set up processing of item
         var u3 = u2
-                .onItem().invoke(()-> {booleans[0] = true; printWithThreadId(".onItem()");})
-                .onSubscription().invoke(() -> { booleans[1] = true; printWithThreadId(".onSubscription()"); });
+                .onItem().invoke(() -> {
+                    booleans[0] = true;
+                    printWithThreadId(".onItem()");
+                })
+                .onSubscription().invoke(() -> {
+                    booleans[1] = true;
+                    printWithThreadId(".onSubscription()");
+                });
 
         //use it
         var startAt = System.currentTimeMillis();
-        u3.subscribe().with(item -> {booleans[2] = true; integerResult = item; printWithThreadId(".subscribe()"); });
+        u3.subscribe().with(item -> {
+            booleans[2] = true;
+            integerResult = item;
+            printWithThreadId(".subscribe()");
+        });
         var finishAt = System.currentTimeMillis();
 
         //main thread is not blocked
         int sum = 0;
-        for(int i = 0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             try {
                 Thread.sleep(30);
             } catch (InterruptedException e) {
@@ -171,7 +185,6 @@ public class MutinyScratchPad {
 
 
     }
-
 
 
 }
